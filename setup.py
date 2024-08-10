dc16ddbb42b6407f8e5c21690de41d6183b0009b
adamritter <58403584+adamritter@users.noreply.github.com>
2024-08-10 16:20:38 +0200

    0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,18 @@ module = Extension('fasttag', sources=['fasttag/fasttag.c'])
 
 setup(
     name='fasttag',
-    version='1.0',
-    description='A simple example module written in C.',
+    version='0.1',
+    description='Extremely fast HTML tag generator',
     ext_modules=[module],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Adam Ritter',
+    author_email='aritter@gmail.com',
+    url='https://github.com/adamritter/fasttag',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
